ea091690ebd302632f848b74868fdc4b3e8241e1
cotoflux <guaupelu@hotmail.com>
2020-05-22 10:35:36 +0200

    Corregido error en el try catch

--- src/misVideos/Uso_clases.java
+++ src/misVideos/Uso_clases.java
@@ -61,27 +61,21 @@ public class Uso_clases {
 		do {
 		
         try{
-        	if(numeroIncorrecto==false) {
-        		System.out.println("ES FALSE");
-        	}else if(numeroIncorrecto==true) {
-        		System.out.println("ES TRUE");
-        	}else {
-        		System.out.println("No es ni true ni false");
-        	}
+
     		System.out.println("Tell me what do you want to do");
     		System.out.println("Escribe un numero entero, si escribes otra cosa que no sea un numero saltara la excepcion: ");
     		System.out.println("1. Register");
     		System.out.println("2. Login");
     		System.out.println("3. Exit");
-    		respuesta= entradas.nextInt();
+    		respuesta= Integer.parseInt(entradas.nextLine());
         	System.out.println("El numero decimal escrito ha sido "+respuesta);
-        	numeroIncorrecto=true;
+
         	numeroIncorrecto=false;
             
-        }catch(InputMismatchException e){
+        }catch(NumberFormatException e){
         	 
              System.out.println("Error, ha introducido un valor incorrecto");
-             numeroIncorrecto=false;
+             numeroIncorrecto=true;
         }
 		}while(numeroIncorrecto==true);
 		
